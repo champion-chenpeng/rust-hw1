538ef949046ab9758a332a2c227e93741b21609d
marincao <marincao626@gmail.com>
2024-01-28 22:06:13 -0800

    empty implementation of part3

--- ocean/src/beach.rs
+++ ocean/src/beach.rs
@@ -7,20 +7,18 @@ use std::slice::Iter;
 #[derive(Debug)]
 pub struct Beach {
     // TODO: Declare the fields of the Beach struct here.
-	pub crabs: Vec<Crab>,
-	pub clansystem: ClanSystem,
 }
 
 impl Beach {
     pub fn new() -> Beach {
-        Beach { crabs: Vec::new(), clansystem: ClanSystem::new() }
+        unimplemented!();
     }
 
     /**
      * Returns the number of crabs on the beach.
      */
     pub fn size(&self) -> usize {
-        self.crabs.len()
+        unimplemented!();
     }
 
     /**
@@ -31,15 +29,15 @@ impl Beach {
      *     - The newly added crab should be at the END of the collection.
      */
     pub fn add_crab(&mut self, crab: Crab) {
-        self.crabs.push(crab);
+        unimplemented!();
     }
 
     pub fn get_crab(&self, index: usize) -> &Crab {
-        &self.crabs[index]
+        unimplemented!();
     }
 
     pub fn crabs(&self) -> Iter<Crab> {
-        self.crabs.iter()
+        unimplemented!();
     }
 
     /**
@@ -48,36 +46,15 @@ impl Beach {
      *   - Some of a reference to the Crab with the highest speed.
      */
     pub fn get_fastest_crab(&self) -> Option<&Crab> {
-		let mut highest_speed = 0;
-		let mut fastest_crab: Option<&Crab> = None;
-		for crab in &self.crabs {
-			if crab.speed() > highest_speed {
-				fastest_crab = Some(&crab);	
-				highest_speed = crab.speed;
-			}
-		}
-		fastest_crab
+        unimplemented!();
     }
 
     /**
      * Returns a vector of references to the crabs with a given name.
      */
     pub fn find_crabs_by_name(&self, name: &str) -> Vec<&Crab> {
-        let mut crabs_with_name = Vec::new();
-		for crab in &self.crabs {
-			if crab.name() == name {
-				crabs_with_name.push(crab);
-			}
-		}
-		crabs_with_name
+        unimplemented!();
     }
-	
-	pub fn breed(&self, i: usize, j: usize, name: String) -> Crab {
-		let speed = 1;
-		let color = Color::cross(self.get_crab(i).color(), self.get_crab(j).color());
-		let diet = Diet::random_diet();
-		Crab::new(name, speed, color, diet)
-	}
 
     /**
      * Breeds the `Crab`s at indices `i` and `j`, adding the new `Crab` to
@@ -85,17 +62,14 @@ impl Beach {
      * the method should panic.
      */
     pub fn breed_crabs(&mut self, i: usize, j: usize, name: String) {
-        if i >= self.crabs.len() || j >= self.crabs.len() {
-			panic!("i or j out of the range!");
-		}
-		self.crabs.push(self.breed(i, j, name));
+        unimplemented!();
     }
 
     /**
      * Returns a reference to the clan system associated with the beach.
      */
     pub fn get_clan_system(&self) -> &ClanSystem {
-        &self.clansystem
+        unimplemented!();
     }
 
     /**
@@ -103,7 +77,7 @@ impl Beach {
      * A crab can only belong to one clan.
      */
     pub fn add_member_to_clan(&mut self, clan_id: &str, crab_name: &str) {
-		self.clansystem.add_member_to_clan(clan_id, crab_name);
+        unimplemented!();
     }
 
     /**
@@ -111,33 +85,6 @@ impl Beach {
      * Return `None` if there are no clear winners between two different existing clans. If the inputs are invalid, return an Err string.
      */
     pub fn get_winner_clan(&self, id1: &str, id2: &str) -> Result<Option<String>, String> {
-		let option1 = self.clansystem.clans.get(id1); 
-		let option2 = self.clansystem.clans.get(id2); 
-		if !option1.is_none() && !option2.is_none() {
-			let names1 = &self.clansystem.get_clan_member_names(id1);
-			let names2 = &self.clansystem.get_clan_member_names(id2);
-			let average1 = self.average_speed(names1);
-			let average2 = self.average_speed(names2);
-			if average1 > average2 {
-				Ok(Some(id1.to_string().clone()))
-			} else if average1 < average2 {
-				Ok(Some(id2.to_string().clone()))
-			} else {
-				Ok(None)
-			}
-		} else {
-			Err(String::from("invalid id!"))
-		}
+        unimplemented!();
     }
-
-
-	pub fn average_speed(&self, names: &Vec<String>) -> f64 {
-		let mut sum = 0;
-		for name in names {
-			for crab in &self.find_crabs_by_name(name) {
-				sum += crab.speed();
-			}
-		}
-		sum as f64 / names.len() as f64
-	}
-}	
+}
--- ocean/src/clans.rs
+++ ocean/src/clans.rs
@@ -1,72 +1,39 @@
-use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct ClanSystem {
     // TODO: add necessary fields
-	pub clans: HashMap<String, Vec<String>>,
 }
 
 impl ClanSystem {
     pub fn new() -> ClanSystem {
-        ClanSystem { clans: HashMap::new() }
+        unimplemented!();
     }
 
     /**
      * Returns a list of the names of the clan members for the given clan id.
      */
     pub fn get_clan_member_names(&self, clan_id: &str) -> Vec<String> {
-        let mut name_list = Vec::new();
-		if let Some(clan) = self.clans.get(clan_id) {
-			for name in clan {
-				name_list.push(name.clone()); 
-			}
-		}
-		name_list
+        unimplemented!();
     }
 
     /**
      * Returns the number of clans currently in existence.
      */
     pub fn get_clan_count(&self) -> usize {
-        self.clans.len()
+        unimplemented!();
     }
 
     /**
      * Returns the number of clan members for the given clan id.
      */
     pub fn get_clan_member_count(&self, clan_id: &str) -> usize {
-//        if let Some(clan) = self.clans.get(clan_id) {
-//			clan.len()
-//		} else {
-//			0
-//		}
-		match self.clans.get(clan_id) {
-			None => 0,
-			Some(clan) => clan.len(),
-		}
+        unimplemented!();
     }
 
     /**
      * Returns the id of the clan with the most number of members, or None if such a clan does not exist.
      */
     pub fn get_largest_clan_id(&self) -> Option<String> {
-        let mut largest_id: Option<String> = None;
-		let mut largest_number = 0;
-		for (clan_id, clan) in &self.clans {
-			if clan.len() > largest_number {
-				largest_id = Some(clan_id.clone());
-				largest_number = clan.len();
-			}
-		}
-		largest_id
+        unimplemented!();
     }
-	
-	pub fn add_member_to_clan(&mut self, clan_id: &str, crab_name: &str) {
-		// here we do not want return any thing so match will be wordy
-		if let Some(clan) = self.clans.get_mut(clan_id) {
-			clan.push(String::from(crab_name));
-		} else {
-			self.clans.insert(clan_id.to_string().clone(), vec![String::from(crab_name)]);
-		}
-	}
 }
